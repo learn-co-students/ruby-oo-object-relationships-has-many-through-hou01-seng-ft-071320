0954f8903632a84325b4706b1a17b85d731926e1
Wilmar Reyes <reyes.wilmar@gmail.com>
2020-07-18 18:44:29 -0500

    Done.

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,33 @@
+require 'pry'
+
 class Customer
-  
+    attr_reader :name, :age
+    @@all = []
+
+    def initialize(name, age)
+        @name = name
+        @age = age
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def new_meal(waiter_name, total, tip)
+        new_meal = Meal.new(waiter_name, self, total, tip)
+        new_meal
+    end
+
+    def meals
+        Meal.all.find_all {|meals| meals.customer == self}
+    end
+
+    def waiters
+        array= []
+        self.meals.find_all do |meal|
+            array << meal.waiter
+        end
+        array
+    end
 end
--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,35 @@
-class Waiter
+require 'pry'
 
+class Waiter
+    attr_reader :waiter, :exp
+    @@all = []
+
+    def initialize(waiter_name, exp)
+        @waiter = waiter_name
+        @exp = exp
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def new_meal(customer_name, total, tip)
+        new_meal = Meal.new(self, customer_name, total, tip)
+        new_meal
+    end
+
+    def meals
+        Meal.all.find_all {|meals| meals.waiter == self}
+    end
+
+    def best_tipper
+        array = []
+        self.meals.select {|i| array << i.tip}
+        self.meals.find do |i|
+            if array.max{|a,b| a<=>b} == i.tip
+                return i.customer
+            end
+        end
+    end
 end
